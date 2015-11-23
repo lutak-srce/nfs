2168e66b412344400d796bc59abdb99cab1912d5
Zaviša Nikač <znikac@srce.hr>
2015-11-23 14:58:02 +0100

    Initial commit haraldsk-nfs-1.1.1 patched

---
+++ manifests/server/export/configure.pp
@@ -0,0 +1,16 @@
+define nfs::server::export::configure (
+  $ensure = 'present',
+  $clients
+) {
+
+  if $ensure != 'absent' {
+    $share = $name
+    # $line = "${name} ${clients}\n"
+
+    concat::fragment { "${name}":
+      target  => '/etc/exports',
+      content => template('nfs/exports_line.erb'),
+      # content => "${line}"
+    }
+  }
+}
