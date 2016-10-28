fe2f435ea4c9684b744372af420b5c9fe226f031
Zavisa Nikac <znikac@srce.hr>
2016-10-28 21:12:31 +0200

    support for Debian Jessie

--- manifests/server/debian.pp
+++ manifests/server/debian.pp
@@ -9,24 +9,4 @@ class nfs::server::debian(
     nfs_v4_idmap_domain => $nfs_v4_idmap_domain,
   }
 
-  package { 'nfs-kernel-server':
-      ensure => 'installed',
-  }
-
-  if nfs::server::debian::nfs_v4 == true {
-    service {
-      'nfs-kernel-server':
-        ensure    => running,
-        subscribe => [
-          Concat['/etc/exports'],
-          Augeas['/etc/idmapd.conf', '/etc/default/nfs-common']
-          ],
-    }
-  } else {
-    service {
-    'nfs-kernel-server':
-      ensure    => running,
-      subscribe => Concat['/etc/exports'],
-    }
-  }
 }
--- manifests/server/redhat.pp
+++ manifests/server/redhat.pp
@@ -24,23 +24,51 @@ class nfs::server::redhat::install {
 
 class nfs::server::redhat::service {
 
-  if nfs::server::redhat::nfs_v4 == true {
-      service {"nfs":
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        hasstatus  => true,
-        require    => Package["nfs-utils"],
-        subscribe  => [ Concat['/etc/exports'], Augeas['/etc/idmapd.conf'] ],
+  if $nfs::server::redhat::nfs_v4 == true {
+      case $facts['os']['release']['major'] {
+        '7'     : { 
+          service {"nfs-server":
+            ensure     => running,
+            enable     => true,
+            hasrestart => true,
+            hasstatus  => true,
+            require    => Package["nfs-utils"],
+            subscribe  => [ Concat['/etc/exports'], Augeas['/etc/idmapd.conf'] ],
+         }
+         }
+        default : {
+          service {"nfs":
+            ensure     => running,
+            enable     => true,
+            hasrestart => true,
+            hasstatus  => true,
+            require    => Package["nfs-utils"],
+            subscribe  => [ Concat['/etc/exports'], Augeas['/etc/idmapd.conf'] ],
+          }
+        }
       }
     } else {
-      service {"nfs":
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        hasstatus  => true,
-        require    => Package["nfs-utils"],
-        subscribe  => Concat['/etc/exports'],
-     }
+      case $facts['os']['release']['major'] {
+        '7'     : { 
+          service {"nfs-server":
+            ensure     => running,
+            enable     => true,
+            hasrestart => true,
+            hasstatus  => true,
+            require    => Package["nfs-utils"],
+            subscribe  => Concat['/etc/exports'],
+         }
+         }
+        default : {
+          service {"nfs":
+            ensure     => running,
+            enable     => true,
+            hasrestart => true,
+            hasstatus  => true,
+            require    => Package["nfs-utils"],
+            subscribe  => Concat['/etc/exports'],
+          }
+        }
+      }
   }
 }
