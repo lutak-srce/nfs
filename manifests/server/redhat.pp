176ea5ab0a4cf46a2af9048d342f1943b30b7486
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-08-06 10:09:08 +0200

    Parametrize client in server class

--- manifests/server/redhat.pp
+++ manifests/server/redhat.pp
@@ -1,11 +1,14 @@
 class nfs::server::redhat(
   $nfs_v4 = false,
-  $nfs_v4_idmap_domain = undef
+  $nfs_v4_idmap_domain = undef,
+  $client = true,
 ) {
 
-  class{ 'nfs::client::redhat':
-    nfs_v4              => $nfs_v4,
-    nfs_v4_idmap_domain => $nfs_v4_idmap_domain,
+  if $client {
+    class{ 'nfs::client::redhat':
+      nfs_v4              => $nfs_v4,
+      nfs_v4_idmap_domain => $nfs_v4_idmap_domain,
+    }
   }
 
   include nfs::server::redhat::install, nfs::server::redhat::service
