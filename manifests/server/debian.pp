d6b231a2daabcf18ecddd771bcc9b56ac2bd384e
eimamagi <emir.imamagic@gmail.com>
2022-01-26 10:13:22 +0100

    Disable client on nfs::server::debian.

--- manifests/server/debian.pp
+++ manifests/server/debian.pp
@@ -2,11 +2,13 @@
 class nfs::server::debian(
   $nfs_v4 = false,
   $nfs_v4_idmap_domain = undef
+  $client = true,
 ) {
 
-  class{ 'nfs::client::debian':
-    nfs_v4              => $nfs_v4,
-    nfs_v4_idmap_domain => $nfs_v4_idmap_domain,
+  if $client {
+    class{ 'nfs::client::debian':
+      nfs_v4              => $nfs_v4,
+      nfs_v4_idmap_domain => $nfs_v4_idmap_domain,
+    }
   }
-
 }
